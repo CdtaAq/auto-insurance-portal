b228bf72d1719a42f9e335b5922ab64317c09b0a
chris Taylor <ctay456a@gmail.com>
2025-05-25 04:01:14 -0400

    Create InsuranceServiceApplication.java

---
+++ service/src/main/java/.../InsuranceServiceApplication.java
@@ -0,0 +1,6 @@
+@SpringBootApplication
+public class InsuranceServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceServiceApplication.class, args);
+    }
+}
